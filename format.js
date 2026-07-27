2091de8c06fa4eefea11484af9113e6424841b77
iki1uc <entertrainer@web.de>
2026-07-27 08:01:46 +0200

    Create format.js

---
+++ format.js
@@ -0,0 +1,8 @@
+[
+  { "key": "core", "value": "RESPO_CORE" },
+  { "key": "scan9", "value": "RESPO_SCAN9" },
+  { "key": "axis7", "value": "RESPO_AXIS7" },
+  { "key": "line2", "value": "RESPO_LINE2" },
+  { "key": "ghost", "value": "RESPO_GHOST" },
+  { "key": "nc-check", "value": true }
+]
