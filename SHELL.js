de9ee213e0513c47d9f9005d27768d7ed298012e
iki1uc <entertrainer@web.de>
2026-08-17 03:23:34 +0200

    Update SHELL.js

--- SHELL.js
+++ SHELL.js
@@ -1,1 +1,11 @@
+export const SHELL = {
+    name: "SHELL",
+    type: "RIZ",
+    role: "Stabilisierung",
+    vector: "shield",
+    drive: "-T",
+    layer: "SYS",
+    diag: "S→W",
+    status: "aktiv"
+};
 
