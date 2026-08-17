64748e225373f1ddf2959d12597ccfe77ef6ae41
iki1uc <entertrainer@web.de>
2026-08-17 03:23:07 +0200

    Update DOOR.js

--- DOOR.js
+++ DOOR.js
@@ -1,1 +1,11 @@
+export const DOOR = {
+    name: "DOOR",
+    type: "RIZ",
+    role: "Öffnung",
+    vector: "open",
+    drive: "-T",
+    layer: "SYS",
+    diag: "W→S",
+    status: "aktiv"
+};
 
