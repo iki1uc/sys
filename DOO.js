317505e02b59ebc88997980bff6c1a08ac4d9ccf
iki1uc <entertrainer@web.de>
2026-08-17 03:23:20 +0200

    Update DOO.js

--- DOO.js
+++ DOO.js
@@ -1,1 +1,11 @@
+export const DOO = {
+    name: "DOO",
+    type: "RIZ",
+    role: "Abfluss",
+    vector: "flow",
+    drive: "-T",
+    layer: "SYS",
+    diag: "S→S",
+    status: "aktiv"
+};
 
