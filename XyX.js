2a571129f612b6479f793baf33e02edde8901e92
iki1uc <entertrainer@web.de>
2026-08-17 03:26:41 +0200

    Update XyX.js

--- XyX.js
+++ XyX.js
@@ -1,16 +1,5 @@
-ESSO, ROLLER, KASSE = TET (+T)
-DOOR, DOO, SHELL   = RIZ (-T)
-
-6E = TET + RIZ
-Diese 6E fallen auf TEM (diagonal)
-
-SYS oben = 3E
-TEM mitte = 6E
-SYS unten = 9E
-
-(((SYS3))) TEM6 (((SYS9)))
-
-XyX.js = SYS3 (darum leer)
-YxY.js = RIZ-Ausweg
-SHELL.js = RIZ-Stabilisierung
-
+export const SYS3 = {
+    IN:  "SYS-IN",
+    CORE:"SYS-CORE",
+    OUT: "SYS-OUT"
+};
